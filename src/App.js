dfffbf5d9b636b44f2046741b6a1d4a709d42a98
borikanes <ooludemi@terpmail.umd.edu>
2018-01-30 02:09:05 -0500

    redoing the header the right way......at least i think its the right way

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Topheader.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
+        <header className="App-header">
+          < TopHeader />
+        </header>
         <h1 className="App-body">
           Save the Date!
         </h1>
@@ -22,3 +22,24 @@ class App extends Component {
 }
 
 export default App;
+
+
+class TopHeader extends Component {
+  render() {
+    return(
+      <div className="TopHeader">
+        <div className="Top-row-div">
+          <h5 className="Top-row-div-date-location">September 22, 2018</h5>
+          <h5 className="Top-row-div-date-location">College Park, Maryland</h5>
+          <h5 className="Top-row-div-hashtag">#MalloryAndBori</h5>
+        </div>
+        <div className="Header-title-dummy">
+          <h1>Mallory & Bori</h1>
+        </div>
+        <div className="Header-title">
+          <h1>Mallory & Bori</h1>
+        </div>
+      </div>
+    );
+  }
+}
