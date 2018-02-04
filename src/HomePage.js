3eda0fba2451fe2011153f519893153b9322c039
vhh167 <oluwabori.oludemi@capitalone.com>
2018-02-04 18:18:10 -0500

    now working on the middle section of the home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,13 +3,17 @@ import 'font-awesome/css/font-awesome.css';
 import './HomePage.css';
 
 export default class HomePage extends Component {
-
     render() {
         return (
             <div className="Homepage-top-image-div">
                 <div className="Homepage-top-image"></div>
                 <div className="Homepage-top-image-text-div">
                     <h1>We're getting married!</h1>
+                </div>
+                <div className="Homepage-mid-base-div">
+                    <div className="Homepage-mid-innerbase-div">
+
+                    </div>
                 </div>
             </div>
         );
