c7aa41a2453c8ed89ca619f7ccc4dd4bd4dd99c1
borikanes <ooludemi@terpmail.umd.edu>
2018-02-08 00:51:43 -0500

    re made the smaller corner pictures and started doing some flex stuff

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import 'font-awesome/css/font-awesome.css';
 import './HomePage.css';
 
+var ampersandSpanStyle = {
+    fontSize: '60px',
+    color: 'white',
+    alignItems: 'center'
+};
+
+
 export default class HomePage extends Component {
+
     render() {
         return (
             <div className="Homepage-top-image-div">
@@ -15,7 +23,19 @@ export default class HomePage extends Component {
                         <div className="Homepage-mid-top-left-arc-div"></div>
                         <div className="Homepage-mid-top-right-arc-div"></div>
                         <div className="Homepage-mid-name-div">
-                            
+                            <div className="Homepage-mid-mallory-div">
+                                <h1> 
+                                    Mallory 
+                                    <span> McDonald </span>
+                                </h1>
+                            </div>
+                            <span>&</span>
+                            <div className="Homepage-mid-bori-div">
+                                <h1> 
+                                    Oluwabori
+                                    <span> Oludemi </span>
+                                </h1>
+                            </div>
                         </div>
                         <div className="Homepage-mid-bottom-left-arc-div"></div>
                         <div className="Homepage-mid-bottom-right-arc-div"></div>
