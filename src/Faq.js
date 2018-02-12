029e6d1f1ec804cde07be4948729b2faac486aec
borikanes <ooludemi@terpmail.umd.edu>
2018-02-12 08:55:56 -0500

    starting with routing

---
+++ src/Faq.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.css';
+import './Faq.css';
+
+export default class Faq extends Component {
+    render(){
+        return(
+            <div className="Faq-parent-div" >
+
+            </div>
+        );
+    }
+}
