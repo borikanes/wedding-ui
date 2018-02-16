8deb02ad589466ae9930101ca1eaf92aadbb60b5
borikanes <ooludemi@terpmail.umd.edu>
2018-02-16 01:33:21 -0500

    created midsection component file

---
+++ src/MidSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.css';
+import './MidSection.css';
+
+export default class MidSection extends Component {
+
+    render(){
+        return(
+            <div className="MidSection-base-div" >
+
+            </div>
+        );
+    }
+}
--- src/TopicHeader.js
+++ src/TopicHeader.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import 'font-awesome/css/font-awesome.css';
 import './TopicHeader.css';
 import Footer from './Footer.js';
+import MidSection from './MidSection.js';
 
 export default class TopicHeader extends Component {
 // style={{height: '140.5%'}}
     render(){
+        var midStyle = {
+            // height: {this.props.height}
+        }
         return(
-            <div className="TopicHeader-parent-div">
+            <div style={midStyle} className="TopicHeader-parent-div">
                 <div className="TopicHeader-header-parent-div">
                     <div className="TopicHeader-header-content-div">
                         <h1>{this.props.title}</h1>
                         <p>{this.props.detail}</p>
                     </div>
                 </div>
-                <Footer />
+                <MidSection />
+                {/* <Footer /> */}
             </div>
         );
     }
