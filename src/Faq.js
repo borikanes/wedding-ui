9e77f1d83d2af3064f73c84c4f5578509e4fe854
borikanes <ooludemi@terpmail.umd.edu>
2018-02-19 03:03:24 -0500

    figured out mid section, still need to refactor. Also added sketch file for the save the date

--- src/Faq.js
+++ src/Faq.js
@@ -7,16 +7,18 @@ import './HomePage.css';
 
 export default class Faq extends Component {
     render(){
+        // let innerDivHeight = (window.matchMedia("(max-width: 767px)").matches) ? {'height': 660 + 'px'} : {'height': 960 + 'px'}
         return(
             <div className="Faq-parent-div">
                 <TopicHeader height="90%" title="FAQs" detail="Common questions you may have about our wedding. Feel free to message us if you have more questions!"/>
                 <div className="Faq-body-content-div" >
                     <div className="Faq-mid-top-left-arc-div"></div>
                     <div className="Faq-mid-top-right-arc-div"></div>
-                    <div>
+                    <div /*style={innerDivHeight}*/ className="Faq-inner-div" >
 
                     </div>
-
+                    <div className="Faq-mid-bottom-left-arc-div"></div>
+                    <div className="Faq-mid-bottom-right-arc-div"></div>
                 </div>
             </div>
         );
