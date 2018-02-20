e31349c7b1d4c471bf36c75c45bd50b4051222be
borikanes <ooludemi@terpmail.umd.edu>
2018-02-20 02:04:03 -0500

    doing some circle stuff on the registry page

--- src/Registry.js
+++ src/Registry.js
@@ -2,11 +2,65 @@ import React, { Component } from 'react';
 import 'font-awesome/css/font-awesome.css';
 import './Registry.css';
 import TopicHeader from './TopicHeader.js';
+import Footer from './Footer.js';
 
 export default class Registry extends Component {
     render(){
+        var heightValue;
+        if (window.matchMedia("(max-width: 700px)").matches) {
+            heightValue = '170'
+        } else if(window.matchMedia("(max-width: 600px)").matches){
+            heightValue = '150'
+        } else {
+            heightValue = '180'
+        }
         return(
-            <TopicHeader title="Registry" detail="Check out our registry!" />
+            <div className="MidSection-parent-div">
+                <TopicHeader title="Registry" detail="Check out our registry!" />
+                <div className="MidSection-body-content-div" >
+                    <div className="MidSection-mid-top-left-arc-div"></div>
+                    <div className="MidSection-mid-top-right-arc-div"></div>
+                    <div className="MidSection-inner-div" >
+                        <div className="Registry-parent-div" >
+                            <div className="Registry-left-div" >
+                                <TextInCircle height={heightValue} width={heightValue}/>
+                            </div>
+                            <div className="Faq-vertical-line-enclosing-div" >
+                                <div className="Faq-vertical-line-div" ></div>
+                            </div>
+                            <div className="Registry-information-div" >
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="MidSection-mid-bottom-left-arc-div"></div>
+                    <div className="MidSection-mid-bottom-right-arc-div"></div>
+                </div>
+                <div className="MidSection-footer-divider-div" ></div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+class TextInCircle extends Component {
+    render(){
+        // var 
+        var circleStyle = {
+            height: this.props.height + 'px',
+            width: this.props.width + 'px',
+            borderRadius: '50%',
+            alignItems: 'center',
+            backgroundColor: '#FFD700',
+            justifyContent: 'center',
+            display: 'flex',
+            flexDirection: 'column'
+        };
+        return(
+            <div style={circleStyle}>
+                <p>For</p>
+                <p style={{marginTop: '0'}}>Mallory and Bori</p>
+            </div>
         );
     }
 }
