61e24e8ea5451c6e5cf4d16e641e63c4b91787fb
borikanes <ooludemi@terpmail.umd.edu>
2018-03-01 01:57:39 -0500

    i think im done with the wedding party page may need to test out bigger text and see what happens

--- src/WeddingParty.js
+++ src/WeddingParty.js
@@ -8,6 +8,12 @@ import Avatar from 'react-avatar';
 
 export default class WeddingParty extends Component {
     render(){
+        // let parentDivHeight
+        // if (window.matchMedia("(max-width: 700px)").matches) {
+        //     parentDivHeight = "600px"
+        // } else {
+        //     parentDivHeight = "200px"
+        // }
         return(
             <div className="MidSection-parent-div">
                 <TopicHeader title="Wedding Party" detail="Meet our friends" />
@@ -15,24 +21,42 @@ export default class WeddingParty extends Component {
                     <div className="MidSection-mid-top-left-arc-div"></div>
                     <div className="MidSection-mid-top-right-arc-div"></div>
                     <div className="MidSection-inner-div" >
-                        <WeddingPartyProfile height="200px" leftName="TUMI OLUDEMI" rightName="RACHEL POISALL"
+                        <WeddingPartyProfile /*height={parentDivHeight}*/ leftName="TUMI OLUDEMI" rightName="RACHEL POISALL"
                         leftDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test" 
                         rightDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test"
-                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966" 
-                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966" />
+                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" 
+                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" />
                         <div className="WeddingParty-question-answer-space-div"></div>
-                        <WeddingPartyProfile height="200px" leftName="TUMI OLUDEMI" rightName="RACHEL POISALL"
+                        <WeddingPartyProfile /*height={parentDivHeight}*/ leftName="MIKE FERRACO" rightName="ALLISON SEELEY"
                         leftDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test" 
                         rightDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test"
-                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966" 
-                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966" />
+                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" 
+                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" />
                         <div className="WeddingParty-question-answer-space-div"></div>
-                        <WeddingPartyProfile height="200px" leftName="TUMI OLUDEMI" rightName="RACHEL POISALL"
+                        <WeddingPartyProfile /*height={parentDivHeight}*/ leftName="JOSHUA ODUMOSU" rightName="JENNY POISALL"
                         leftDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test" 
                         rightDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test"
-                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966" 
-                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966" />
+                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" 
+                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" />
                         <div className="WeddingParty-question-answer-space-div"></div>
+                        <WeddingPartyProfile /*height={parentDivHeight}*/ leftName="CHRIS LONGE" rightName="RACHEL ABREMSKI"
+                        leftDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test" 
+                        rightDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test"
+                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" 
+                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" />
+                        <div className="WeddingParty-question-answer-space-div"></div>
+                        <WeddingPartyProfile /*height={parentDivHeight}*/ leftName="OPE OSUNKOYA" rightName="RACHEL ABREMSKI"
+                        leftDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test" 
+                        rightDescription="I'm the most awesome person ever but not really this is just a placeholder for text to test"
+                        leftSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" 
+                        rightSrc="https://www.gravatar.com/avatar/7bcbb578177aaa9846709fb8d8732966?s=200" />
+                        <div className="WeddingParty-question-answer-space-div"></div>
+                        <div className="WeddingParty-question-answer-space-div"></div>
+                        <div className="Wedding-mid-initials-div">
+                            <p>M</p>
+                            <div className="Wedding-mid-initials-vertical-line-div" ></div>
+                            <p>B</p>
+                        </div>
                     </div>
                     <div className="MidSection-mid-bottom-left-arc-div"></div>
                     <div className="MidSection-mid-bottom-right-arc-div"></div>
@@ -46,11 +70,20 @@ export default class WeddingParty extends Component {
 
 class WeddingPartyProfile extends Component {
     render(){
+        let leftAvatar 
+        if (this.props.leftName === "TUMI OLUDEMI") {
+            leftAvatar = <Avatar className="Wedding-avatar-style" facebookId="1527056551" size={100} round={true} />
+        } else if(this.props.leftName === "JOSHUA ODUMOSU") {
+            leftAvatar = <Avatar className="Wedding-avatar-style" facebookId="844108345" size={100} round={true} />
+        } else {
+            leftAvatar = <Avatar className="Wedding-avatar-style" size={100} round={true} src={this.props.leftSrc} />
+        }
+        
         return (
-            <div style={{'height': this.props.height}} className="Wedding-question-answer-parent-div">
+            <div /*style={{'height': this.props.height}}*/ className="Wedding-question-answer-parent-div">
                 <div className="Wedding-left-profile-div">
                     <div className="Wedding-avatar-style" >
-                        <Avatar className="Wedding-avatar-style" size={100} round={true} src={this.props.leftSrc} />
+                        {leftAvatar}
                     </div>
                     <h1>{this.props.leftName}</h1>
                     <p>Groomsman</p>
@@ -66,6 +99,9 @@ class WeddingPartyProfile extends Component {
                     <h1>{this.props.rightName}</h1>
                     <p>Bridesmaid</p>
                     <p>{this.props.rightDescription}</p>
+                </div>
+                <div className="Wedding-vertical-line-small-enclosing-div" >
+                    <div className="Wedding-vertical-line-small-div" ></div>
                 </div>
             </div>
         );
