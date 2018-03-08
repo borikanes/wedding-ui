3544b932ae0a36e9df8aebe9238b947750c8c3cf
borikanes <ooludemi@terpmail.umd.edu>
2018-03-08 00:19:22 -0500

    continuing locations page

--- src/Locations.js
+++ src/Locations.js
@@ -15,7 +15,28 @@ export default class Locations extends Component {
                     <div className="MidSection-mid-top-right-arc-div"></div>
                     <div /*style={innerDivHeight}*/ className="MidSection-inner-div" >
                         <div className="Locations-parent-div" >
-
+                            <div className="Locations-church-div" >
+                                <h1>Church</h1>
+                                <p>Some church</p>
+                                <p>Church ceremony starts at 11am</p>
+                                <p>1600 Pennsylvania Ave, Some City, MD 22222</p>
+                                <button className="Locations-direction-button" >Direction</button>
+                            </div>
+                            <div className="Locations-vertical-line-enclosing-div" >
+                                <div className="Locations-vertical-line-div" ></div>
+                            </div>
+                            <div className="Locations-reception-div" >
+                                <h1>Reception</h1>
+                                <p>Some Hall's Placeholder</p>
+                                <p>Reception starts at 2pm. You're welcome to arrive early</p>
+                                <p>1600 Pennsylvania Avenue, Some City, MD 22222</p>
+                                <button className="Locations-direction-button" >D i r e c t i o n</button>
+                            </div>
+                        </div>
+                        <div className="Locations-initials-div">
+                            <p>M</p>
+                            <div className="Locations-initials-vertical-line-div" ></div>
+                            <p>B</p>
                         </div>
                     </div>
                     <div className="MidSection-mid-bottom-left-arc-div"></div>
