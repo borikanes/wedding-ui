c25e2870fd5f89a9bdf99d8af60a289a6b28b6f6
borikanes <ooludemi@terpmail.umd.edu>
2018-03-09 01:11:22 -0500

    works like magic, done with adding passcode to protect location content

--- src/Locations.js
+++ src/Locations.js
@@ -6,47 +6,122 @@ import Footer from './Footer.js';
 import './Locations.css';
 
 export default class Locations extends Component {
+    constructor(props) {
+        super(props);
+        this.handlePasswordSubmit = this.handlePasswordSubmit.bind(this);
+        this.state = {
+            isAllowedToSeeContent: false,
+            passwd: '',
+            isCodeWrong: false
+        };
+    }
+
+    handlePinButtonClick() {
+        this.setState({
+            isAllowedToSeeContent: true
+        });
+    }
+
+    handlePasswordTextChange = (event) => {
+        this.setState({
+            passwd: event.target.value,
+            isCodeWrong: false
+        });
+    };
+
+    handlePasswordSubmit() {
+        if (this.state.passwd.toLowerCase() === process.env.REACT_APP_LOCATION_CODE) {
+            this.setState({
+                isAllowedToSeeContent: true
+            });
+        } else {
+            this.setState({
+                isCodeWrong: true
+            });
+        }
+    }
+
     render() {
-        return (
-            <div className="MidSection-parent-div">
-                <TopicHeader title="Locations" detail="These are the venues the wedding will be taking place" />
-                <div className="MidSection-body-content-div" >
-                    <div className="MidSection-mid-top-left-arc-div"></div>
-                    <div className="MidSection-mid-top-right-arc-div"></div>
-                    <div /*style={innerDivHeight}*/ className="MidSection-inner-div" >
-                        <div className="Locations-parent-div" >
-                            <div className="Locations-church-div" >
-                                <h1>Church</h1>
-                                <p>Some church</p>
-                                <p>Church ceremony starts at 11am</p>
-                                <p>1600 Pennsylvania Ave, Some City, MD 22222</p>
-                                <button onClick={()=> window.open('https://goo.gl/maps/jGxfRMpbvpS2')} className="Locations-direction-button" >D i r e c t i o n</button>
+        var errorParagraph;
+        var inputErrorState = {}
+        if (this.state.isCodeWrong) {
+            errorParagraph = <p style={{'color': 'red', 'fontSize': '15px'}} >Sorry, the code you entered is wrong. Reach out to bori or mallory if you don't know the code</p>
+            inputErrorState = {
+                'border': '1px solid red'
+            }
+        }
+
+        if (this.state.isAllowedToSeeContent) {
+            return (
+                <div className="MidSection-parent-div">
+                    <TopicHeader title="Locations" detail="These are the venues the wedding will be taking place" />
+                    <div className="MidSection-body-content-div" >
+                        <div className="MidSection-mid-top-left-arc-div"></div>
+                        <div className="MidSection-mid-top-right-arc-div"></div>
+                        <div className="MidSection-inner-div" >
+                            <div className="Locations-parent-div" >
+                                <div className="Locations-church-div" >
+                                    <h1>Church</h1>
+                                    <p>Some church</p>
+                                    <p>Church ceremony starts at 11am</p>
+                                    <p>1600 Pennsylvania Ave, Some City, MD 22222</p>
+                                    <button onClick={()=> window.open('https://goo.gl/maps/jGxfRMpbvpS2')} className="Locations-direction-button" >D i r e c t i o n</button>
+                                </div>
+                                <div className="Locations-vertical-line-enclosing-div" >
+                                    <div className="Locations-vertical-line-div" ></div>
+                                </div>
+                                <div className="Locations-reception-div" >
+                                    <h1 style={{'marginTop': 0, 'marginBottom': 0}}>Reception</h1>
+                                    <h2 style={{'marginTop': 0, 'marginBottom': 0}} >&</h2>
+                                    <h2 style={{'marginTop': 0}}>Nigerian ceremony</h2>
+                                    <p>Some Hall's Placeholder</p>
+                                    <p>Reception starts at 2pm. You're welcome to arrive early</p>
+                                    <p>1600 Pennsylvania Avenue, Some City, MD 22222</p>
+                                    <button onClick={()=> window.open('https://goo.gl/maps/jGxfRMpbvpS2')} className="Locations-direction-button" >D i r e c t i o n</button>
+                                </div>
                             </div>
-                            <div className="Locations-vertical-line-enclosing-div" >
-                                <div className="Locations-vertical-line-div" ></div>
-                            </div>
-                            <div className="Locations-reception-div" >
-                                <h1 style={{'marginTop': 0, 'marginBottom': 0}}>Reception</h1>
-                                <h2 style={{'marginTop': 0, 'marginBottom': 0}} >&</h2>
-                                <h2 style={{'marginTop': 0}}>Nigerian ceremony</h2>
-                                <p>Some Hall's Placeholder</p>
-                                <p>Reception starts at 2pm. You're welcome to arrive early</p>
-                                <p>1600 Pennsylvania Avenue, Some City, MD 22222</p>
-                                <button onClick={()=> window.open('https://goo.gl/maps/jGxfRMpbvpS2')} className="Locations-direction-button" >D i r e c t i o n</button>
+                            <div className="Locations-initials-div">
+                                    <p>M</p>
+                                    <div className="Locations-initials-vertical-line-div" ></div>
+                                    <p>B</p>
                             </div>
                         </div>
-                        <div className="Locations-initials-div">
-                            <p>M</p>
-                            <div className="Locations-initials-vertical-line-div" ></div>
-                            <p>B</p>
-                        </div>
+                        <div className="MidSection-mid-bottom-left-arc-div"></div>
+                        <div className="MidSection-mid-bottom-right-arc-div"></div>
                     </div>
-                    <div className="MidSection-mid-bottom-left-arc-div"></div>
-                    <div className="MidSection-mid-bottom-right-arc-div"></div>
+                    <div className="MidSection-footer-divider-div" ></div>
+                    <Footer />
                 </div>
-                <div className="MidSection-footer-divider-div" ></div>
-                <Footer />
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div className="MidSection-parent-div">
+                    <TopicHeader title="Locations" detail="These are the venues the wedding will be taking place" />
+                    <div className="MidSection-body-content-div" >
+                        <div className="MidSection-mid-top-left-arc-div"></div>
+                        <div className="MidSection-mid-top-right-arc-div"></div>
+                        <div className="MidSection-inner-div" >
+                            <div className="Locations-passwd-parent-div" >
+                                <p>To view the wedding locations, please enter the code we sent you. It's in your wedding invite email. Reach out to Bori or Mallory if you can't find it</p>
+                                {errorParagraph}
+                                <div>
+                                    <input style={inputErrorState} className="Locations-input" type="text" placeholder="Enter code here..." value={this.state.passwd} onChange={this.handlePasswordTextChange} />
+                                    <button onClick={this.handlePasswordSubmit} className="Locations-input-button" >S u b m i t</button>
+                                </div>
+                            </div>
+                            <div className="Locations-initials-div">
+                                <p>M</p>
+                                <div className="Locations-initials-vertical-line-div" ></div>
+                                <p>B</p>
+                            </div>
+                        </div>
+                        <div className="MidSection-mid-bottom-left-arc-div"></div>
+                        <div className="MidSection-mid-bottom-right-arc-div"></div>
+                    </div>
+                    <div className="MidSection-footer-divider-div" ></div>
+                    <Footer />
+                </div>
+            );
+        }
     }
 }
